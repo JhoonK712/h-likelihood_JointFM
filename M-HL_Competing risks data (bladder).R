b013a1e4f3ec0379e9e19d831e3a4f7295fe3db6
JhoonK712 <dhekfwl0712@gmail.com>
2026-04-30 12:52:54 +0900

    Add files via upload

---
+++ M-HL_Competing risks data (bladder).R
@@ -0,0 +1,124 @@
+###---------------------------------------------- Compare 3 methods in 3 cases ----------------------------------------------###
+rm(list=ls())
+
+library(frailtyHL)
+library(frailtyEM)
+library(joint.Cox)
+library(numDeriv)
+library(Copula.surv)
+library(ggplot2)
+library(dplyr)
+setwd("C:\\directory")
+source("source_code.R")
+
+###------------------------------------- Competing risks survival data - bladder data -------------------------------------###
+data(bladder)
+center_count <- as.data.frame(table(bladder$center))
+names(center_count) <- c("center", "center_n")
+
+bladder2 <- merge(bladder, center_count, by.x = "center", by.y = "center", all.x = TRUE)
+bladder_sorted <- bladder2[order(bladder2$center_n), ]
+head(bladder_sorted)
+
+names(bladder_sorted)[names(bladder_sorted) == "surtime"] <- "time"
+bladder_sorted$id <- as.numeric(factor(bladder_sorted$center, levels = unique(bladder_sorted$center)))
+
+dataset <- bladder_sorted
+attach(dataset)
+
+P <- dim(dataset)[1]
+EVENT <-matrix(0, P, 4)
+
+EVENT[,1] <- as.matrix(dataset$time, P, 1) # t_event1
+EVENT[,2] <- as.matrix(dataset$time, P, 1) # t_event2
+EVENT[,3] <- as.matrix(ifelse(dataset$status==1, dataset$status, 0), P, 1) # event1
+EVENT[,4] <- as.matrix(ifelse(dataset$status==2, 1, 0), P, 1) # event2
+
+t_event1 <- EVENT[,1]
+t_event2 <- EVENT[,2]
+event1 <- EVENT[,3]
+event2 <- EVENT[,4]
+
+t1max <- max(t_event1)
+t1min <- min(t_event1)
+t2max <- max(t_event2)
+t2min <- min(t_event2)
+
+formula <-  ~ CHEMO + AGE + (1|id)
+
+X <- model.matrix(formula)
+X <- data.frame(X)
+X <- as.matrix(X)
+Z <- as.matrix(X[, c(2:(dim(X)[2]-1))])
+
+G <- length(unique(dataset$id))
+group <- dataset$id
+
+#------------------------------------------- M-HL -------------------------------------------#
+p <- dim(X)[2]-2
+init <- c(rep(0, 2), rep(0, 2), rep(0.0, 5), rep(0.0, 5), log(0.1), 1, 1) # beta1, beta2, spline1, spline2, eta, kappa1, kappa2
+
+res31 <- comp_HL_gamma1(formula, EVENT, init, seed=335)
+res31
+
+#------------------------------------------- random effect figure -------------------------------------------#
+v_h <- res31[[9]]
+SE <- res31[[10]]
+
+#lower (upper) bound
+LB <- v_h-1.96*SE
+UB <- v_h+1.96*SE 
+
+CI <- cbind(LB, UB) #confidence interval (CI)
+
+x <- 1:length(unique(dataset$id))
+
+result <- cbind(x, v_h, CI)
+
+comp_random_figure <- ggplot(result, aes(x = x, y = v_h) ) +
+		geom_point(size = 1.5) +
+		geom_errorbar(aes(ymin = LB, ymax = UB), width = 0.2) +
+		geom_hline(yintercept = 0) +
+		labs(x = "Centre number", y = "Estimated center effects") +
+		theme_minimal()+
+		theme(
+			panel.grid.major = element_blank(),
+			panel.grid.minor = element_blank(),
+			panel.border = element_rect(fill = NA, size = 1),
+			panel.background = element_blank()
+		)
+
+#------------------------------------------- baseline hazard figure -------------------------------------------#
+# Estimate
+res_g1 <- res31$Est_g1
+res_g2 <- res31$Est_g2
+
+# Setting M-spline
+time <- seq(0, t1max, 1)
+Ms <- M.spline(time, 0, t1max)
+
+# Plot
+esti_g1 <- Ms%*%t(res_g1)	# cmprskCox.reg()
+esti_g2 <- Ms%*%t(res_g2)	# cmprskCox.reg()
+
+ylim <- c(0, 0.0030)
+
+df1 <- data.frame(Time = time, Hazard = esti_g1, Group = "Recurrence")
+df2 <- data.frame(Time = time, Hazard = esti_g2, Group = "Death")
+plot_data <- rbind(df1, df2)
+plot_data$Group <- factor(plot_data$Group, levels = c("Recurrence", "Death"))
+
+comp_base_figure <- ggplot(plot_data, aes(x = Time, y = Hazard, color = Group)) +
+	geom_line(size = 1) +
+	scale_color_manual(values = c("Recurrence" = "black", "Death" = "red")) +
+	scale_x_continuous(breaks = seq(0, max(plot_data$Time), by = 1000), expand = c(0, 0)) + 
+	scale_y_continuous(breaks = seq(0, ylim[2], by = 0.0005), limits = ylim, expand = c(0, 0)) +
+	labs(x = "Time (days)", y = "Baseline hazard") +
+	theme_classic() +
+	theme(
+		legend.position = c(0.95, 0.95),
+		legend.justification = c("right", "top"),
+		legend.title = element_blank(),
+		legend.background = element_blank()
+	)
+
